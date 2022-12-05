c96a6a4d205e8c3aa72117caa1a73173676feee4
nsemirdev <nsemirdev@gmail.com>
2022-12-05 17:05:09 +0300

    initialize i-am-rich app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Emir Alkal on 5.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
