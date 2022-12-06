b42bd5aa3f749d935d39ffac00963fc2218b1e12
nsemirdev <nsemirdev@gmail.com>
2022-12-06 19:55:03 +0300

    initial setup

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Emir Alkal on 6.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
