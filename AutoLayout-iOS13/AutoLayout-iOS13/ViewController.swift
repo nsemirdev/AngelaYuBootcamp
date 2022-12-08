36663e790da0822d796dd6bdd8cf1240d2f271d1
nsemirdev <nsemirdev@gmail.com>
2022-12-09 00:53:56 +0300

    BUG: button is not clickable

--- AutoLayout-iOS13/AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13/AutoLayout-iOS13/ViewController.swift
@@ -14,13 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
+        print("clicked")
         let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-        
+
         diceImageView1.image = allDice[Int.random(in: 0...5)]
         diceImageView2.image = allDice[Int.random(in: 0...5)]
-        
+
     }
-    
 }
 
